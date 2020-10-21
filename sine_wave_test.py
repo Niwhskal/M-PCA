5e7e04169a1bac5fe51e495344e2297bb5195e32
test13234 <lakshwin045@gmail.com>
2020-10-21 20:22:08 +0530

    Added dataset options

--- sine_wave_test.py
+++ sine_wave_test.py
@@ -2,23 +2,42 @@
 
 import os
 import numpy as np
+import time
 import matplotlib.pyplot as plt
 import hdbscan
-from sklearn.cluster import DBSCAN
+from sklearn.cluster import DBSCAN, KMeans
+from sklearn.decomposition import PCA
+import hdbscan
 import seaborn as sns
-# Sine wave data
+import argparse
 
+parser = argparse.ArgumentParser(description='MPCA modelling of the sine wave')
+
+parser.add_argument('-d', '--dataset', default='sine', choices= ['sine', 'cosine'])
+
+parser.add_argument('-c', '--clus', default = 'hdb', choices= ['hdb', 'db','db-hdb', 'kmeans'])
+
+parser.add_argument('-a', '--alg', default='SPCA', choices=['kPCA', 'AE', 'Arima'])
+
+args = parser.parse_args()
+
+#wave data
 freq = 8e03
 cycles = 5
-
-
 sample_freq = 8e03
 
 x = np.arange(sample_freq)
-y = np.sin(2* np.pi*cycles *x / freq)
+
+if (args.dataset == 'sine'):
+	y = np.sin(2* np.pi*cycles *x / freq)
+
+else:
+	y = np.sin(2*np.pi*cycles *x /freq + np.pi/2)
+
 noise = np.random.normal(0,0.12,8000)
 y_noise = y+noise
-#plt.scatter(x,y_noise)
+
+#plt.scatter(x,y_noise, s=1)
 #plt.show()
 
 
