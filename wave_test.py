e3bc404fed58bea1553c1715cdaaf166e3fd2252
test13234 <lakshwin045@gmail.com>
2020-10-29 20:36:05 +0530

    added autoencoder

--- wave_test.py
+++ wave_test.py
@@ -7,13 +7,18 @@ import matplotlib.pyplot as plt
 import hdbscan
 from sklearn.cluster import DBSCAN, KMeans
 from sklearn.decomposition import PCA, KernelPCA
+from sknn.ae import Layer, AutoEncoder
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
 import hdbscan
 import seaborn as sns
 import argparse
 
 parser = argparse.ArgumentParser(description='MPCA modelling of the sine wave')
 
-parser.add_argument('-d', '--dataset', default='sine', choices= ['sine', 'cosine'])
+parser.add_argument('-d', '--dataset', default='sine', choices= ['sine', 'cosine', 'noise'])
 
 parser.add_argument('-c', '--clus', default = 'hdb', choices= ['hdb', 'db','db-hdb', 'kmeans'])
 
@@ -25,11 +30,11 @@ args = parser.parse_args()
 
 tw = tuple(args.arglist)
 epsilon, min_samp, min_csize, cents = args.arglist
-
+np.random.seed(19234)
 fig, ax = plt.subplots(3)
 #wave data
 freq = 8e03
-cycles = 5
+cycles = 10
 sample_freq = 8e03
 
 x = np.arange(sample_freq)
@@ -37,8 +42,12 @@ x = np.arange(sample_freq)
 if (args.dataset == 'sine'):
 	y = np.sin(2* np.pi*cycles *x / freq)
 
-else:
+elif(args.dataset== 'cosine'):
 	y = np.sin(2*np.pi*cycles *x /freq + np.pi/2)
+
+else:
+	nse1 = np.random.randn(len(x))
+	y = np.sin(2*np.pi*cycles*x) +nse1 
 
 noise = np.random.normal(0,0.12,8000)
 y_noise = y+noise+10
@@ -83,7 +92,7 @@ for name in list(set(clustering.labels_)):
 		err.append(1/len(blob_inv) * np.sum(np.sqrt(blob_train, blob_inv)))
 		ax[1].scatter(keys, blob_inv, s=1, label = name)
 		
-print('Error = {}'.format(sum(err)))
+print('multi pca Error = {}'.format(sum(err)))
 ax[1].legend()
 
 #kpca
@@ -94,7 +103,37 @@ if args.alg == 'kPCA':
 	inv_alg = kpca.inverse_transform(trfmd_alg)
 	ax[2].scatter(x, inv_alg, s=1)
 	kerr =1/len(inv_alg) * np.sum(np.sqrt(y_fit, inv_alg))		
-	print('Error = {}'.format(kerr))
+	print('kernel pca error = {}'.format(kerr))
+
+#autoencoder
+
+if args.alg=='AE':
+	ip = torch.tensor(y_fit, requires_grad =True, dtype=torch.float)
+	class autoencoder(nn.Module):
+		
+		def __init__(self):
+			super(autoencoder, self).__init__()
+			
+			self.lin1 = nn.Linear(1, 10)
+			self.lin2 = nn.Linear(10,1)
+		def forward(self, x):
+			x = F.tanh(self.lin1(x))
+			x = F.tanh(self.lin2(x))
+			return x
+
+	ae = autoencoder()
+	optim = optim.SGD(ae.parameters(), lr = 0.01) 	
+	lmet = nn.MSELoss()
+	for ep in range(10):
+		t_loss = 0.0
+		optim.zero_grad()
+		preds = ae(ip)
+		loss = lmet(ip, ip)
+		loss.backward()
+		optim.step()
+		t_loss += loss
+	ax[2].scatter(x, preds.detach().numpy(), s=1)
+	print("Autoencoder error = {}".format(t_loss.item()))
 
 plt.show()
 
